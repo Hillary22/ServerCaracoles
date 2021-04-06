37b99b07bf4b18506b97132fcde634345a691bb8
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2021-04-06 17:09:49 -0500

    server

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,22 @@
 package main;
 
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		PApplet.main("main.Main");
+	}
 
+	public void settings() {
+		size(700, 500);
+	}
+	public void setup() {
+		
+	}
+	public void draw() {
+		
 	}
 
 }
