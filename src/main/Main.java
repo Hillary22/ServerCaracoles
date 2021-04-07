6d4a3a3dbc2bc5a2ee5abac3c471377927130c21
danieldiaz-2502 <daniel_diazvalencia@outlook.com>
2021-04-06 19:13:51 -0500

    Imagenes cargadas

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet{
+	
+	PImage background,caracol1,caracol2,tonelada,velo;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,6 +14,11 @@ public class Main extends PApplet{
 
 	public void settings() {
 		size(700, 500);
+		background = loadImage("../img/bg.png");
+		caracol1 = loadImage("../img/p1.png");
+		caracol2 = loadImage("../img/p2.png");
+		tonelada = loadImage("../img/tonelada.png"); 
+		velo = loadImage("../img/velo.png"); 
 	}
 	public void setup() {
 		
